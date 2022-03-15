abb591f5d865dbecb0d2c0693b0e797b12a04ab0
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-03-16 00:13:35 +0700

    Fix Github Pages Path Stuff

---
+++ web/postbuild.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+/* eslint-disable no-console */
+
+/// <reference lib="es2021" />
+
+import fs from "fs/promises";
+import { globbySync } from "globby";
+
+/**
+ * @param {string} fileName
+ */
+async function GHPagesSvkMahou(fileName) {
+  const result = (await fs.readFile(fileName))
+    .toString()
+    .replaceAll("/_app/", "./_app/");
+
+  await fs.writeFile(fileName, result);
+
+  console.log(`Done with ${fileName}!`);
+}
+
+const fileList = globbySync("build/**/*.html");
+
+console.log(`Items: ${fileList}`);
+
+fileList.map((fname) => GHPagesSvkMahou(fname));
